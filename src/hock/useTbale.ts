d69f62f22e2bea97a2a45c304008d7359958adbc
WIN-C1QH7J5CVNC\EDZ <215200924@qq.com>
2020-09-25 18:58:48 +0800

    no message

--- src/hock/useTbale.ts
+++ src/hock/useTbale.ts
@@ -1,11 +1,35 @@
 import { reactive } from "vue";
-interface data {
-  getData: (url: string) => void;
+interface obj {
+  getData: (callback: (params: params) => void) => void;
+  data:
+    | {
+        [propName: string]: any;
+      }
+    | undefined;
+  total: number;
+}
+interface params {
+  current: number;
+  pageSize: number;
+  [propName: string]: any;
 }
 
-export default function useData(url: string): data {
-  const getData = (url) => {};
+export default function useData(callback: () => any, params: params): obj {
+  let data;
+  let total = 0;
+  const getData = async (callback: (params: params) => any) => {
+    let res = await callback(params);
+    if (res.code == "0") {
+      data = res.data;
+      total = res.data.total;
+      params.loading = false;
+    }
+  };
+  getData(callback);
+
   return {
     getData,
+    data,
+    total,
   };
 }
